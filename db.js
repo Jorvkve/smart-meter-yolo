c91a0b0290bfd21a0d9e0b8375aa2a9cc8eead0e
Jorvkve <Turth00@gmail.com>
2026-05-28 17:40:44 +0700

    Use MySQL connection pool

--- db.js
+++ db.js
@@ -1,20 +1,25 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
   database: "smart_meter_db",
   port: 3306,
-  dateStrings: true
+  dateStrings: true,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect(err => {
+db.getConnection((err, connection) => {
   if (err) {
     console.log("DB Error:", err);
-  } else {
-    console.log("MySQL Connected ✅");
+    return;
   }
+
+  console.log("MySQL Pool Connected ✅");
+  connection.release();
 });
 
 module.exports = db;
